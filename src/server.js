de2ddd756f9eea587c35c6d5193cea5c184f248b
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2021-07-04 15:21:34 -0500

    return

--- src/server.js
+++ src/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
+const exphbs = require("express-handlebars");
+
 //inicializaciones
 const app = express();
 //settings
-app.set('port', process.env.PORT || 4000)
-app.set('views', path.join(__dirname,'views'))
+app.set("port", process.env.PORT || 4000)
+app.set("views", path.join(__dirname,"views"));
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    latyoutsDir: path.join(app.get("views"),"layouts"),
+    partialsDir: path.join(app.get("views"),"partials"),
+    extname: ".hbs"
+}));
+app.set("view engine",".hbs");
 //Middlewares
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 
 //Global variables
 
 // Routes
-app.get('/', (req,res)=>res.send("Hello world"))
+app.use(require("./routes/index.routes"))
 //Static Files
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,"public")))
 
 module.exports = app;
