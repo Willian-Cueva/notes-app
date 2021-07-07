e18e75b9ec9b047a6c6f710cc5cbccad08cd34f2
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2021-07-07 13:59:58 -0500

    mensajes crud con flash

--- src/controllers/notes.controllers.js
+++ src/controllers/notes.controllers.js
@@ -2,38 +2,40 @@ const notesCtrl = {};
 const Note = require('../models/Note');
 
 notesCtrl.renderNotesForm = (req, res) => {
-  res.render("notes/new-note");
+  res.render('notes/new-note');
 };
 
 notesCtrl.createNewNote = async (req, res) => {
   const {title,description} =req.body;
   const newNote = new Note({title,description});
   await newNote.save();
+  req.flash('success_msg','Nota Añadida Satisfactoriamente');
   res.redirect('/notes');
 };
 
 notesCtrl.renderNotes = async (req, res) => {
-  // res.send("Renderizar Notas");
+  // res.send('Renderizar Notas');
   const notes = await Note.find().lean();
-  res.render("notes/all-notes",{notes});
+  res.render('notes/all-notes',{notes});
 };
 
 notesCtrl.renderEditForm = async (req, res)=> {
     const note = await Note.findById(req.params.id).lean();
     const id = req.params.id;
-    res.render("notes/edit-note",{note,id});
+    res.render('notes/edit-note',{note,id});
 };
 
-notesCtrl.updateForm = (req,res)=>{
+notesCtrl.updateForm = async (req,res)=>{
     const {id,title,description}= req.body;
-    console.log(req.body);
-    console.log(id);
-    res.send("Se actualizaron los datos");
+    await Note.findByIdAndUpdate(id,{title,description});
+    req.flash('success_msg','Nota Actualizada Satisfactoriamente');
+    res.redirect('/notes');
 }
 
 notesCtrl.deleteNote = async (req,res)=> {
     await Note.findByIdAndDelete(req.params.id);
-    res.redirect("/notes");
+    req.flash('success_msg','Nota Eliminada Satisfactoriamente');
+    res.redirect('/notes');
 }
 
 module.exports = notesCtrl;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //inicializaciones
 const app = express();
@@ -20,8 +22,17 @@ app.set('view engine','.hbs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'secrect',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 //Global variables
-
+app.use((req,res,next)=>{
+    res.locals.success_msg = req.flash('success_msg');
+    next();
+})
 // Routes
 app.use(require('./routes/index.routes'))
 app.use(require('./routes/notes.routes'))
