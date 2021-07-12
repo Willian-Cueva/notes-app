351016de76bdd5917ca982850ac173cba1039383
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2021-07-12 14:34:01 -0500

    cambiando datos en la base de datos xd

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const {NOTES_APP_MONGODB_HOST,NOTES_APP_MONGODB_DATABASE} = process.env;
+const NOTES_APP_MONGODB_HOST = "localhost";
+const NOTES_APP_MONGODB_DATABASE = "notes-app";
 
 const MONGODB_URI=`mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
