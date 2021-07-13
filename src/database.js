03d6a7b29380b851623b1672799507f2493dab51
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2021-07-13 11:00:54 -0500

    arreglando link database

--- src/database.js
+++ src/database.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const NOTES_APP_MONGODB_HOST = "localhost";
-const NOTES_APP_MONGODB_DATABASE = "notes-app";
+const MONGODB_URI =
+  "mongodb+srv://clusterwin:123clusterwin1@cluster0.p1swo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const MONGODB_URI="mongodb+srv://clusterwin:<123clusterwin1>@cluster0.p1swo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(MONGODB_URI, {
+mongoose
+  .connect(MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true
-}) .then(db => console.log("La base de datos está conectada"))
-    .catch(db => console.log(err))
+    useCreateIndex: true,
+  })
+  .then((db) => console.log("La base de datos está conectada"))
+  .catch(err => console.log(err));
